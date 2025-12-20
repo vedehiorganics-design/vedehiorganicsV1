2ecfa19ea79fc8927f51e682e7c907d829bd033c
vedehiorganics-design <vedehiorganics@gmail.com>
2025-12-20 19:08:40 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,17 @@
-import { ViteSSG } from "vite-ssg";
+/*import { ViteSSG } from "vite-ssg";
 import App from "./App";
 import { routes } from "./routes";
 import "./index.css";
 
 export const createApp = ViteSSG(App, { routes });
+*/
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
