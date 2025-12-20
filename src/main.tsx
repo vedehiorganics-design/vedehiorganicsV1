5cf38e2ed6b4c4c34567bb8d9475b441ce32e271
vedehiorganics-design <vedehiorganics@gmail.com>
2025-12-20 18:56:19 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,6 @@
-/*import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
--------------
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
-import App from "./App";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
-  </React.StrictMode>
-);
-*/
-
 import { ViteSSG } from "vite-ssg";
-import App from "./App.tsx";
+import App from "./App";
 import { routes } from "./routes";
+import "./index.css";
 
-export const createApp = ViteSSG(
-  App,
-  { routes },
-
-);
+export const createApp = ViteSSG(App, { routes });
