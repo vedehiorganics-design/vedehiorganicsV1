65706b91d76b17e7a42541b4d8acb20257a0e1c2
vedehiorganics-design <vedehiorganics@gmail.com>
2025-12-20 19:08:04 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import { RouterView } from "vite-ssg";
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-emerald-50 to-white">
-      <RouterView />
-    </div>
-  );
+  return <Home />;
 }
