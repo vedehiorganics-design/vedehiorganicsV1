c87c71a27919113960624607ebc3e0a4e5da9f77
vedehiorganics-design <vedehiorganics@gmail.com>
2025-12-20 18:24:01 +0530

    Updated routes.ts

---
+++ routes.ts
@@ -0,0 +1,8 @@
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+
+export const routes = [
+  { path: "/", component: Home },
+];
