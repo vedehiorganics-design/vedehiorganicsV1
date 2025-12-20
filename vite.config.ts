f8edcef816dd5b6002ab9534b07caddf64f11699
vedehiorganics-design <vedehiorganics@gmail.com>
2025-12-20 18:19:10 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,6 @@ import prerender from "vite-plugin-prerender";
 
 export default defineConfig({
   plugins: [
-    react(),
-    prerender({
-      routes: [
-        "/",          // Home
-      ],
-      renderer: "@prerenderer/renderer-react",
-      postProcess(renderedRoute) {
-        // Ensure clean URLs
-        if (renderedRoute.route.endsWith("/")) {
-          renderedRoute.html = renderedRoute.html.replace(
-            /<a href="\/index.html"/g,
-            '<a href="/"'
-          );
-        }
-        return renderedRoute;
-      }
-    })
+    react()
   ]
 });
