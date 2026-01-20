9d6cc5da25ce33009692d495d2e581620e8c8b92
vedehi organics <vedehiorganics@gmail.com>
2026-01-20 11:50:06 +0530

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -268,7 +268,8 @@ If you are looking for fresh organic oyster mushrooms grown naturally in India, 
                           </div>
                           <div>
                               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.1427226213013!2d73.79088469999999!3d18.5676032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2bf88cec26845%3A0x767b5e36ada2f00c!2sVedehi%20Organics!5e0!3m2!1sen!2sin!4v1768889876955!5m2!1sen!2sin" 
-                                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                              </iframe>
                           </div>
                       </div>
                   </div>
